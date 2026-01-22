7b539e3529bd339074cbe9571a3134adabaad71d
Kashish Tomar <kashish@gmail.com>
2026-01-22 23:27:23 +0530

    Basic Express server setup
    Signed-off-by: Kashish Tomar <kashish@gmail.com>

---
+++ server.js
@@ -0,0 +1,6 @@
+const express= require('express');
+const app=express();
+const PORT= 3000;
+app.listen(PORT, () =>{
+console.log("Server is up and running on port 3000! Ready to handle requests.")
+});
